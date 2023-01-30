b2b6fbe6f2ab689275acc69337b74f096a6de29c
Andromeda <geraskin1997@gmail.com>
2023-01-30 23:35:42 +0200

    done

---
+++ README.md
@@ -0,0 +1,56 @@
+# Aws serverless project
+
+## Run project
+To get this project working you need:
+
+```
+        1. Create s3 bucket
+        2. Create file secret.py in a root directory
+        3. Input these constants into file
+    
+            aws_access_key_id = <your access key>
+            aws_secret_access_key = <your secret access key>
+            region_name = <your region>
+            bucket_name = <your bucket name>
+    
+        4. in file serverless.yml change in functions/upload_file_event/events/s3/bucket your name of bucket
+        5. Create a custom layer and upload dependencies.zip into it, see https://docs.aws.amazon.com/lambda/latest/dg/configuration-layers.html 
+        6. Add layer to each lambda function, see https://docs.aws.amazon.com/lambda/latest/dg/invocation-layers.html
+
+```
+   
+Open working directory and type this into terminal window
+```
+$ serverless deploy
+```
+You will see which endpoints and functions are available
+## Application endpoints
+
+You can do POST request in order to get link to upload object to s3 bucket.
+
+Example of request body is down here:
+
+```json
+{
+    "file_name": "happy-dog.png",
+    "callback_url": "https://webhook.site/4eaddd11-c612-413d-8f10-5d8168dec959"
+}
+```
+
+```
+params:
+file_name - name of object which will be saved
+callback_url - url which is going to accept POST request passing blob_id using then in GET request 
+```
+
+After you do POST request you get response that is similar to this:
+```json
+{
+    "Url to upload": "https://andrrromeda-bucket.s3.amazonaws.com/sad-dog.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=<acces key>%2F20230130%2F<region>%2Fs3%2Faws4_request&X-Amz-Date=20230130T194526Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=901d153158c21d531d7fd67a8a2cd89faa584bd3675cd5e5f9b7cd72c339368c"
+}
+```
+
+You can upload file to s3 bucket by this link, choosing file and sending PUT request.
+
+After file's successfully uploaded you can do GET request passing blob_id as path parameter to get link to file.
+You can get blob_id from DynamoDB table 'blobs' or having glance on response of callback_url
---
+++ handler.py
@@ -0,0 +1,104 @@
+import json
+import uuid
+import boto3
+
+import requests
+from botocore.config import Config
+
+import secret
+
+dynamoDb = boto3.resource('dynamodb')
+blobs_table = dynamoDb.Table('blobs')
+urls_table = dynamoDb.Table('callback_urls')
+s3_client = boto3.client('s3', config=Config(signature_version='s3v4'),
+                         region_name=secret.region_name, aws_access_key_id=secret.aws_access_key_id,
+                         aws_secret_access_key=secret.aws_secret_access_key)
+bucket_name = secret.bucket_name
+
+
+def build_response(status_code, body=None):
+    response = {
+        'statusCode': status_code,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        }
+    }
+    if body is not None:
+        response['body'] = json.dumps(body)
+
+    return response
+
+
+def save_blob(event, context):
+    request_body = json.loads(event['body'])
+    callback_url = request_body['callback_url']
+    file_name = request_body['file_name']
+
+    item = {
+        'file_name': file_name,
+        'callback_url': callback_url
+    }
+    urls_table.put_item(Item=item)
+    upload_url = create_presigned_url(file_name, 'put_object')
+    return build_response(200, {'Url to upload': upload_url})
+
+
+def create_presigned_url(file_name, client_method, expiration=3600):
+    url = s3_client.generate_presigned_url(
+        ClientMethod=client_method,
+        Params={
+            'Bucket': bucket_name,
+            'Key': file_name
+        },
+        ExpiresIn=expiration
+    )
+    return url
+
+
+def get_blob(event, context):
+    blob_id = event['pathParameters']['blobId']
+    response = blobs_table.get_item(
+        Key={
+            'id': blob_id
+        }
+    )
+
+    if 'Item' in response:
+        download_blob_url = response['Item']['url']
+        return build_response(200, {'download_url': download_blob_url})
+
+    return build_response(404, {'Message': f'There is no blob with id={blob_id}'})
+
+
+def upload_file_event(event, context):
+    key = event['Records'][0]['s3']['object']['key']
+    blob_id = put_blob(key)
+    url_item = urls_table.get_item(
+        Key={
+            'file_name': key
+        }
+    )
+
+    callback_url = url_item['Item']['callback_url']
+    response = {
+        'message': 'Uploading file has been successfully completed.',
+        'blob_id': blob_id
+    }
+    content = requests.post(callback_url, json=response)
+    print(content)
+
+
+def put_blob(object_name):
+    download_url = create_presigned_url(object_name, 'get_object')
+    blob_id = generate_uuid()
+    blob_item = {
+        'id': blob_id,
+        'url': download_url
+    }
+    blobs_table.put_item(Item=blob_item)
+    return blob_id
+
+
+def generate_uuid():
+    return str(uuid.uuid4())
